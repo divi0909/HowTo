5db8ece4ce69e50f1a0660f8df2703e23cf86112
Octal <octal@octals-mac-mini.local>
2016-10-24 12:32:50 +0530

    Initial Commit

---
+++ DemoData/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DemoData
+//
+//  Created by Octal on 24/10/16.
+//  Copyright © 2016 Octal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
